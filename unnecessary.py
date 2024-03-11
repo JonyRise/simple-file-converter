02d41a8c92048544b9914cbafb558adc43bb5536
JonyRise <jony.rise.up.1997@gmail.com>
2024-03-11 21:21:33 +0200

    fix base4d on remarks

--- unnecessary.py
+++ unnecessary.py
@@ -6,24 +6,28 @@
 from os import path
 
 supported_formats = ['jpg', 'png', 'img', 'jpeg']
+supported_format_str = ','.join(supported_formats)
+    
+def convert_file(file_name, new_format):
+    with open(file_name, 'rb') as original_file,\
+    open(f"{original_file.name.split('.')[0]}.{new_format}", 'wb') as formated_file:
+        formated_file.write(original_file.read())
+    print(f"File {original_file.name} successfully converted to {formated_file.name}")
+
 while True:
     file_name = input("Enter file name which you want to convert (if you want quit enter 'q'): ")
     if file_name == 'q':
         break
-    if path.exists(file_name):
-        supported_format_str = ','.join(supported_formats)
-        while True:
-            format = input(f"Enter supported format({supported_format_str}) (if you want quit enter 'q'): ")
-            if format == 'q':
-                break
-            if format in supported_formats:
-                with open(file_name, 'rb') as original_file,\
-                open(f"{original_file.name.split('.')[0]}.{format}", 'wb') as formated_file:
-                    formated_file.write(original_file.read())
-                print(f"File {original_file.name} successfully converted to {formated_file.name}")
-                break
-            else:
-                print(f"this format is not supported!Supported formats:{supported_format_str}")
-    else:
+    if not path.exists(file_name):
         print("File is not exists! Enter correct file name!")
-
+        continue
+    
+    while True:
+        format = input(f"Enter supported format({supported_format_str}) (if you want quit enter 'q'): ")
+        if format == 'q':
+            break
+        if format in supported_formats:
+            convert_file(file_name, format)
+            break
+        else:
+            print(f"this format is not supported!Supported formats:{supported_format_str}")
